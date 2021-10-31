cfef13c2640fbb55ac9705d35f5b9c1db1896a74
Simon Dirmeier <simon.dirmeier@web.de>
2021-10-31 23:06:57 +0100

    Add some more information

--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -22,8 +22,9 @@
 
       <div id="navlist" class="navbar-menu" style="margin-right: 50px">
         <div class="navbar-end">
-          <a class="navbar-item is-tab" href="/#home">Home</a>
-          <a class="navbar-item is-tab" href="/#contact">Contact</a>
+          <a class="navbar-item is-tab" href="/">Home</a>
+          <a class="navbar-item is-tab" href="/consulting">Consulting</a>
+          <a class="navbar-item is-tab" href="/case-studies">Case studies</a>
         </div>
       </div>
     </div>

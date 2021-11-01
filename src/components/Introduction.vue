07968deab91492261ace557f25a7b68f4e45455f
Simon Dirmeier <simon.dirmeier@web.de>
2021-11-01 13:59:28 +0100

    Remove said button

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -9,31 +9,6 @@
       <a href="/case-studies">case studies</a> and occasionally offer
       <a href="/consulting">statistical consulting</a>.
     </p>
-    <div>
-      <div align="right">
-        <button
-          class="button is-light"
-          style="font-size: 0.9rem; padding: 0px;  margin-top: -20px"
-          v-on:click="button_is_pressed = ! button_is_pressed"
-        >
-          <span class="icon is-small">
-            <font-awesome-icon :icon="['fas', 'book-open']" />
-          </span>
-          <span>
-            {{ button_is_pressed ? "Hide" : "Read more" }}
-          </span>
-        </button>
-      </div>
-      <p v-if="button_is_pressed">
-        My expertise and scientific interests lie broadly in probabilistic machine
-        and deep learning, (nonparametric) Bayesian statistics and causal
-        inference.<br />
-
-        I am particularly enthusiastic about <i>probablistic programming</i> which
-        are probabilistic I offer statistical consulting for probabilistic
-        modeling
-      </p>
-    </div>
   </div>
 </template>
 

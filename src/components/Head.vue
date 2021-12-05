bdd00a1d6c08992f98cbb09943ca15c56b7eea01
Simon Dirmeier <simd23@protonmail.com>
2021-12-05 12:40:49 +0100

    Update email

--- src/components/Head.vue
+++ src/components/Head.vue
@@ -14,7 +14,7 @@
         contact @ simon-dirmeier . com
         <div style="font-size: 0.7em; margin-top: 1%">
           <a
-            href="mailto:contact at simon.dirmeier.com"
+            href="mailto:simon.dirmeier @ protonmail com"
             style="color: black; margin-right: 1%; font-size: 0.9em"
           >
             <i class="fa fa-envelope fa-2x" />

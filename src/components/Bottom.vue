5d7cc1af34b70a2c156f364e414960617fceccd8
Simon Dirmeier <simd23@protonmail.com>
2022-09-19 10:25:36 +0200

    Add recent blog posts

--- src/components/Bottom.vue
+++ src/components/Bottom.vue
@@ -3,8 +3,8 @@
     <footer style="margin-top: 15%; float: right">
       <p style="font-size: 0.75em;">
         &copy; 2017-2021 Simon Dirmeier (
-        <a href="mailto:contact @ simon-dirmeier.com"
-          >contact @ simon-dirmeier.com</a
+        <a href="mailto:simon.dirmeier @ protonmail com"
+          >simon.dirmeier @ protonmail com</a
         >)
       </p>
     </footer>

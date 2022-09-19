8e6ce49197d3a03543c2d2a6ffcc059bfaa71cfc
Simon Dirmeier <simd23@protonmail.com>
2022-09-19 10:25:36 +0200

    Add recent blog posts

--- src/components/Consulting.vue
+++ src/components/Consulting.vue
@@ -1,19 +1,26 @@
 <template>
-  <div id='consulting'>
-  <h2>Consulting</h2>
-    <p>
-    I am occasionally available for statistical consulting on probabilistic modeling, causal inference,
-     and research software development. In particular, I offer consulting on
-    <ul>
-      <li>statistical/causal data analysis, e.g., of biomedical, financial, or astrophysical data sets</li>
-      <li>development of web dashboards for visualization</li>
-      <li>implementation of bespoke probabilistic models using PPLs (*)</li>
-      <li>Bayesian workflow, model validation, model selection, etc.</li>
-      <li>implementation of computational pipelines (e.g., of analysis workflows for reproducible research)</li>
-    </ul>
-      (*) via Stan, NumPyro or TensorFlow Probability (in R or Python) <br>
-    To find a sketch of topics that I have expertise with, please have a look at
-    my <a href="/case-studies">case studies</a>.
-  </p>
+  <div id="consulting">
+    <h2>Consulting</h2>
+    <div>
+      I am occasionally available for statistical consulting on probabilistic
+      modeling, causal inference, and research software development. In
+      particular, I offer consulting on
+      <ul>
+        <li>
+          statistical/causal data analysis, e.g., of biomedical, financial, or
+          astrophysical data sets
+        </li>
+        <li>development of web dashboards for visualization</li>
+        <li>implementation of bespoke probabilistic models using PPLs (*)</li>
+        <li>Bayesian workflow, model validation, model selection, etc.</li>
+        <li>
+          implementation of computational pipelines (e.g., of analysis workflows
+          for reproducible research)
+        </li>
+      </ul>
+      (*) via Stan, NumPyro or TensorFlow Probability (in R or Python) <br />
+      To find a sketch of topics that I have expertise with, please have a look
+      at my <a href="/case-studies">case studies</a>.
+    </div>
   </div>
 </template>

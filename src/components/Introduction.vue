593cb6ba083a64a4bc576c7ee61ce427fa48237c
Simon Dirmeier <simd23@protonmail.com>
2022-09-19 18:17:01 +0200

    Update about

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -5,8 +5,8 @@
       Bayesian faible. Currently, I am working as a senior data scientist at the
       <a href="https://datascience.ch/">Swiss Data Science Center</a> in the
       academic cell. Apart from that, I enjoy spending my time developing
-      open-source software and writing <a href="/case-studies">case studies</a>,
-      and I occasionally offer
+      open-source software and writing <a href="/case-studies">case studies</a>.
+      I also occasionally offer
       <a href="/about#consulting">statistical consulting</a>.
     </p>
   </div>

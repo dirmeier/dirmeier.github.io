69db113dbcdac1342c5788f2b9f89054f3dc5608
Simon Dirmeier <simd23@protonmail.com>
2022-10-19 17:36:28 +0200

    Revert "Revert "Move code from Vue to Svelte""
    This reverts commit 613159d446b299772cc7118498d99a1e24373314.

---
+++ src/routes/_navbar.svelte
@@ -0,0 +1,28 @@
+<div>
+	<nav class="navbar has-shadow is-fixed-top" role="navigation" aria-label="main navigation">
+		<div class="container">
+			<div class="navbar-brand">
+				<a
+					role="button"
+					class="navbar-burger"
+					aria-label="menu"
+					aria-expanded="false"
+					data-target="navlist"
+					onclick="document.querySelector('#navlist').classList.toggle('is-active');"
+				>
+					<span aria-hidden="true" />
+					<span aria-hidden="true" />
+					<span aria-hidden="true" />
+				</a>
+			</div>
+
+			<div id="navlist" class="navbar-menu" style="margin-right: 50px">
+				<div class="navbar-end">
+					<a class="navbar-item is-tab" href="/">Home</a>
+					<a class="navbar-item is-tab" href="/case-studies">Case studies</a>
+					<a class="navbar-item is-tab" href="/about">About</a>
+				</div>
+			</div>
+		</div>
+	</nav>
+</div>
---
+++ svelte.config.js
@@ -0,0 +1,28 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-auto';
+
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		})
+	},
+
+	preprocess: [
+		preprocess({
+			postcss: true,
+			scss: {
+				prependData: '@use "src/variables.scss" as *;'
+			}
+		})
+	]
+};
+
+export default config;

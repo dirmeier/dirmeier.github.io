2583f2d7debb1251ded46e75530952a3879f7e85
Simon Dirmeier <simd23@protonmail.com>
2022-11-26 18:46:42 +0100

    New page

--- src/routes/_navbar.svelte
+++ src/routes/_navbar.svelte
@@ -1,7 +1,7 @@
 <div>
 	<div class="container">
 			<div class="navbar-brand" style="margin-top: 4%">
-				<a class="navbar-item is-tab" href="/case-studies">case studies</a>
+				<a class="navbar-item is-tab" href="/">case studies</a>
 				<a class="navbar-item is-tab" href="/code">code</a>
 				<a class="navbar-item is-tab" href="/about">about</a>
 			</div>

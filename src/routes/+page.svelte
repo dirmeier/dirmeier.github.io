280d0593b51e097cc7f0e59052f59e2ccf5742e7
Simon Dirmeier <simd23@protonmail.com>
2022-11-26 18:46:42 +0100

    New page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,20 @@
 <script>
 	import Navv from './_navbar.svelte';
 	import Bottom from './_bottom.svelte';
-	import Page from './_page.svelte';
+	import Page from './case-studies/_page.svelte';
 </script>
 
 <svelte:head>
-	<title>research</title>
+	<title>case studies</title>
 </svelte:head>
+
 <div>
 	<Navv />
 	<div>
 		<section class="section">
 			<div class="container">
 				<div class="columns">
-					<div class="column is-offset-3 is-6">
+					<div class="column is-offset-4 is-6">
 						<Page />
 						<Bottom />
 					</div>

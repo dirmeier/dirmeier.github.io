ce271a5674f27fd1105da329cf7b7f3704f1d5eb
Simon Dirmeier <simd23@protonmail.com>
2023-03-04 19:44:03 +0100

    New page

--- src/routes/_bottom.svelte
+++ src/routes/_bottom.svelte
@@ -1,8 +1,8 @@
 <div class="container">
 	<footer style="margin-top: 15%; float: right">
 		<p style="font-size: 0.75em;">
-			&copy; 2017-2022 Simon Dirmeier (
-			<a href="mailto:simon.dirmeier @ protonmail com">simon.dirmeier @ protonmail com</a>)
+			&copy; 2017-2023 Simon Dirmeier (
+			<a href="mailto:simon dot dirmeier @ protonmail dot com">simon dot dirmeier @ protonmail dot com</a>)
 		</p>
 	</footer>
 </div>

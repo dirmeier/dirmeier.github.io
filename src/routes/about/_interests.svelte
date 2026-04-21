ade26d3b505fe5987f9f4137e2b66d3e168912ba
Simon Dirmeier <simd23@protonmail.com>
2026-04-21 20:07:38 +0200

    -

--- src/routes/about/_interests.svelte
+++ src/routes/about/_interests.svelte
@@ -1,7 +1,7 @@
 <div>
   <h4 class="title is-4">Research interests</h4><div class="columns">
     <div class="column is-9" style=" justify-content: center">
-      Generative modelling, (causal) representation learning, semi-supervised
+      Generative modelling, (causal) representation learning, self-supervised
       learning, time series models, signal processing, numerical computing.
     </div>
   </div>

bf4a6fa6c33c1920fb2561c2074981e7601d963c
Simon Dirmeier <simd23@protonmail.com>
2026-05-26 21:50:08 +0200

    --

--- src/routes/about/_interests.svelte
+++ src/routes/about/_interests.svelte
@@ -1,7 +1,7 @@
 <div>
   <h4 class="title is-4">Research interests</h4><div class="columns">
     <div class="column is-9" style=" justify-content: center">
-      <p>I am currently primarily interested in generative modelling, (causal) representation learning, self-supervised
+      <p>I am currently primarily interested in generative modelling, reinforcement learning, (causal) representation learning, self-supervised
       learning, and numerical computing.</p>      
       <!-- <p>In the past, I was mainly working on causal inference, applied probability (probabilistic modelling, stochastic processes, Gaussian processes, Bayesian nonparametrics, ...),
       approximate and Bayesian inference (Markov Chain Monte Carlo, VI, Simulation-Based Inference), time series models, probabilistic programming, and generative modelling (score and flow matching, normalizing flows).    
